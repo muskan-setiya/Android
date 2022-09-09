085944c5662d74b1c75037084e3eb231c10d5b2e
Muskan Setiya <78153457+muskan-setiya@users.noreply.github.com>
2022-09-09 17:30:26 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,52 +1,14 @@
-package com.example.activity_lifecycle;
+package com.example.layout_practical_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.d("Activity Lifecycle", "Activity Created");
-    }
-
-    @Override
-    protected void onStart(){
-        super.onStart();
-        Log.d("Activity Lifecycle" , "Activity Started");
-    }
-
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        Log.d("Activity Lifecycle" , "Activity Destroyed");
-    }
-
-    @Override
-    protected void onPause(){
-        super.onPause();
-        Log.d("Activity Lifecycle","Activity Paused");
-    }
-
-    @Override
-    protected void onResume(){
-        super.onResume();
-        Log.d("Activity Lifecycle","Activity Resume");
-    }
-
-    @Override
-    protected void onRestart(){
-        super.onRestart();
-        Log.d("Activity Lifecycle","Activity Restarted");
-    }
-
-    @Override
-    protected void onStop(){
-        super.onStop();
-        Log.d("Activity Lifecycle","Activity Stopped");
+        setContentView(R.layout.linear_main);
     }
 }
